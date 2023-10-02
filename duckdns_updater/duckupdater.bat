b15e029b1865da68b80b26a099ffbfbcce0a7152
Jose Porcel <6834673+jmporcelg@users.noreply.github.com>
2023-10-03 00:28:52 +0200

    Initial release for duckdns-updater

---
+++ duckdns_updater/duckupdater.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "pythonw" "__init__.py"
+exit
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="duckdns-updater",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "pystray",
+        "Pillow",
+    ],
+    entry_points={
+        "console_scripts": [
+            "duckdns-updater = duckdns_updater.__init__:main",
+        ],
+    },
+    author="Jose Porcel",
+    description="A DuckDNS IP updater application.",
+    long_description=open('README.md').read(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
